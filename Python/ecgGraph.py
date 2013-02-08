fde467b27552e26239b685802b7f1bc71b00049a
Rachel Liu <rachliuseas@gmail.com>
2013-02-08 14:28:19 -0500

    Python read Serial.write()
    Print works, graph broken

--- Python/ecgGraph.py
+++ Python/ecgGraph.py
@@ -4,31 +4,37 @@ import matplotlib.pyplot as plt
 
 import numpy
 import serial
+from time import sleep
 
-print "step 0"
-ser = serial.Serial(2, timeout=2)
+ser = serial.Serial(6, baudrate=9600, timeout=1)
 ser.setRTS(True)
 ser.setRTS(False)
 
-print "step 1"
+fig = plt.figure()
 
-line1, = plt.plot([], [])
+ax = fig.add_subplot(111)
 
-plt.show()
+line1, = ax.plot([], [], animated=True, lw=2, label="value")
 
-print "step 3"
+fig.show()
+
 def extend(line, xval, yval):
 	line1.set_xdata(numpy.append(line.get_xdata(), xval))
 	line1.set_ydata(numpy.append(line.get_ydata(), yval))
-	plt.draw()
-
-print "step 4"
+	#Draw the lines
+	try:
+        	ax.draw_artist(line1)
+    	except AssertionError:
+        	pass
 
 time = 0
+
 while time < 1000:
-	print str(time)
-	height = ser.readline()
-	print str(height)
+	while ser.inWaiting() < 2:
+		sleep(0.01)
+	bytes = ser.read(2)
+	height = ord(bytes[0]) * 256 + ord(bytes[1])
+	#print time, height
+	extend(line1, time, height)
 	time += 1
-	extend(line1, time, height )
 ser.close
