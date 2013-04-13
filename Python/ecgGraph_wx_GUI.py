b34925735cddbb2b2dd7d35127d23f0273b278e2
Daniel Wei <weyichen@seas.upenn.edu>
2013-04-13 00:51:50 -0400

    We can plot data in the GUI now!
    All we need is to link the serial data getting code and updating code
    with the matplotlib figures and this should be trivial!

--- Python/ecgGraph_wx_GUI.py
+++ Python/ecgGraph_wx_GUI.py
@@ -10,6 +10,7 @@ import random
 import sys
 import matplotlib
 matplotlib.use('WXAgg')
+import matplotlib.pylab as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_wxagg import \
     FigureCanvasWxAgg as FigCanvas, \
@@ -19,6 +20,7 @@ import pylab as pyl
 import serial
 import time
 from collections import deque
+import threading
 
 class MainFrame(wx.Frame):
     def __init__(self, parent, ID, title):
@@ -48,7 +50,7 @@ class MainFrame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
         
-        #create sidebar
+        # Create the main panel which contains the graphing elements
         self.main_panel = MainPanel(self)
         
         # A Statusbar in the bottom of the window
@@ -91,98 +93,81 @@ class MainFrame(wx.Frame):
         self.Close(True)  # Close the frame.
 
 class MainPanel(wx.Panel):
+# this class contains the GUI elements for graphing, and the logic for setting up and updating the graph, plus a sidebar
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
         
+        # this will contain all elements in this class
         mainSizer = wx.BoxSizer(wx.HORIZONTAL)
         
-        # 4x3 grid of all 12 ECG leads
-        pyl.ioff()
-        
+        # "interactive mode" off
+        # if on, figure is redrawn every time it is updated, such as setting data in the extend() method
+        plt.ioff()
+
         # graph figure parameters
         BUF_LEN = 128
         timestep = 1
         period = BUF_LEN * timestep
         height = 2000000
         channels = 12
+        dpi = 100
+        g_width = 3.2
+        g_length = 1.8
         styles = ['r-', 'g-', 'y-', 'm-', 'k-', 'r-', 'g-', 'y-', 'm-', 'k-', 'r-', 'g-']
         
-        # X values
-        times = np.arange(0, period, timestep)
-        # Y values
-        samples = np.zeros([BUF_LEN, channels])
-
-        self.lines = []
+        times = np.arange(0, period, timestep) # X values
+        samples = np.zeros([BUF_LEN, channels]) # Y values
         
-        self.fig = Figure((9.0, 12.0), dpi=100)
-        self.lines.extend(self.fig.add_subplot(1,1,1))
+        # save initial background of all graphing canvas, for use when updating graphs
+        # place data, axes, and backgrounds of matplotlib figures in lists for easy acces to handles later
+        figs = []
+        lines = []
+        axes = []
+        backgrounds = []
         
-        #for i in range (channels):
+        #change size of graph axes labels
+        matplotlib.rc('font', size='10')
+        
+        #-----------------BEGIN GRAPHS------------------
+        
+        # create a 3 by 4 grid of ECG lead graphs
+        graphGrid = wx.GridBagSizer(hgap=5, vgap=10)
+        # create all plots and place them in the graph grid
+        for i in range (channels):
+            # create label for graph and add it to grid
+            label = wx.StaticText(self, label="Lead "+str(i+1))
+            graphGrid.Add(label, pos=((i*2)%6, i/3))
+        
+            # initialize graph figure and add it to grid
+            grafig = Figure((g_width, g_length), dpi) 
+            axis = grafig.add_subplot(111)
+            axis.set_axis_bgcolor('black')
+            line = axis.plot(times, samples[:,i], styles[i], animated=True)
             
-        
-        # self.fig, self.axes = pyl.subplots(nrows=4, ncols=3)
-        # for i in range(channels):
-            # self.lines.extend(self.axes[i/3][i%3].plot(times, samples[:,i], styles[i], animated=True))
-            # self.lines[i].axes.set_ylim(-height, height)
-            # self.lines[i].axes.set_xlim(-timestep, period + timestep)
-        # self.fig = Figure((3.0, 3.0), dpi=self.dpi)
-        self.fig.show()
-        
-        # Let's capture the background of the figure
-        #self.backgrounds = [self.fig.canvas.copy_from_bbox(ax.bbox) for ax in self.axes]
-
-        # We need to draw the canvas before we start animating...
-        self.fig.canvas.draw()
+            
+            
+            canvas = FigCanvas(self, -1, grafig)
+            graphGrid.Add(canvas, pos=((i*2+1)%6, i/3))
+            
+            # add figure elements to the appropriate list
+            figs.append(grafig);
+            lines.extend(line)
+            axes.append(axis);
+            backgrounds.append(grafig.canvas.copy_from_bbox(axis.bbox))
+            
+            lines[i].axes.set_ylim(-height, height)
+            lines[i].axes.set_xlim(-timestep, period + timestep)
+            
+            # We need to draw the canvas before we start animating
+            grafig.canvas.draw()
         
         # Make a convenient zipped list for simultaneous access
-        #self.items = zip(self.lines, self.axes, self.backgrounds)
+        items = zip(figs, lines, axes, backgrounds)
         
-        graphGrid = wx.GridBagSizer(hgap=5, vgap=5)
+        #-----------------BEGIN SIDEBAR-----------------
         
-        # self.init_plot()
-        # self.lead1 = FigCanvas(self, -1, self.fig)
-        # graphGrid.Add(self.lead1, pos=(1,0))
-        
-        # self.init_plot()
-        # self.lead2 = FigCanvas(self, -1, self.fig)
-        # graphGrid.Add(self.lead2, pos=(1,1))
-        # self.init_plot()
-        # self.lead6 = FigCanvas(self, -1, self.fig)
-        # graphGrid.Add(self.lead6, pos=(3,2))
-        # self.init_plot()
-        # self.lead7 = FigCanvas(self, -1, self.fig)
-        # graphGrid.Add(self.lead7, pos=(5,0))
-        # self.init_plot()
-        # self.lead10 = FigCanvas(self, -1, self.fig)
-        # graphGrid.Add(self.lead10, pos=(7,0))
-        
-        # lead1_lbl = wx.StaticText(self, label="Lead 1")
-        # lead2_lbl = wx.StaticText(self, label="Lead 2")
-        # lead3_lbl = wx.StaticText(self, label="Lead 3")
-        # lead4_lbl = wx.StaticText(self, label="Lead 4")
-        # lead5_lbl = wx.StaticText(self, label="Lead 5")
-        # lead6_lbl = wx.StaticText(self, label="Lead 6")
-        # lead7_lbl = wx.StaticText(self, label="Lead 7")
-        # lead8_lbl = wx.StaticText(self, label="Lead 8")
-        # lead9_lbl = wx.StaticText(self, label="Lead 9")
-        # lead10_lbl = wx.StaticText(self, label="Lead 10")
-        # lead11_lbl = wx.StaticText(self, label="Lead 11")
-        # lead12_lbl = wx.StaticText(self, label="Lead 12")
-        # graphGrid.Add(lead1_lbl, pos=(0,0))
-        # graphGrid.Add(lead2_lbl, pos=(0,1))
-        # graphGrid.Add(lead3_lbl, pos=(0,2))
-        # graphGrid.Add(lead4_lbl, pos=(2,0))
-        # graphGrid.Add(lead5_lbl, pos=(2,1))
-        # graphGrid.Add(lead6_lbl, pos=(2,2))
-        # graphGrid.Add(lead7_lbl, pos=(4,0))
-        # graphGrid.Add(lead8_lbl, pos=(4,1))
-        # graphGrid.Add(lead9_lbl, pos=(4,2))
-        # graphGrid.Add(lead10_lbl, pos=(6,0))
-        # graphGrid.Add(lead11_lbl, pos=(6,1))
-        # graphGrid.Add(lead12_lbl, pos=(6,2))
-        
-        # sidebar with buttons and vitals data
-        sideGrid = wx.GridBagSizer(vgap=5)
+        # Sidebar with buttons and vitals data
+        sideBar = wx.GridBagSizer(vgap=5)
         
         # add pause button
         self.paused = False
@@ -197,30 +182,75 @@ class MainPanel(wx.Panel):
         self.heartrate = wx.StaticText(self, label="1 bpm")
         
         # add sidebar elements to grid
-        sideGrid.Add(self.pause_button, pos=(0,0))
-        sideGrid.Add(self.elapsed_lbl, pos=(1,0))
-        sideGrid.Add(self.elapsed, pos=(2,0))
-        sideGrid.Add(self.heartrate_lbl, pos=(3,0))
-        sideGrid.Add(self.heartrate, pos=(4,0))
+        sideBar.Add(self.pause_button, pos=(0,0))
+        sideBar.Add(self.elapsed_lbl, pos=(1,0))
+        sideBar.Add(self.elapsed, pos=(2,0))
+        sideBar.Add(self.heartrate_lbl, pos=(3,0))
+        sideBar.Add(self.heartrate, pos=(4,0))
         
+        #-----------------ENDOF SIDEBAR-----------------
+         
+        # add graph grid and sidebar to sizer, and add that to the panel
         mainSizer.Add(graphGrid, 0, wx.ALL, 5)
-        mainSizer.Add(sideGrid, 0, 0, wx.CENTER)
+        mainSizer.Add(sideBar, 0, 0, wx.CENTER)
         self.SetAutoLayout(1)
         self.SetSizerAndFit(mainSizer)
         
+        # show panel (always last step)
         self.Show(True)
         
+        #-----------------BEGIN GRAPHING LOGIC----------
+        
+        # array to read in 4 bytes at a time
+        data = deque()
+        on = True
+        
+
+        # get and update graph at each position in the grid
+        for j, (fig, line, ax, background) in enumerate(items):
+            fig.canvas.restore_region(background)
+            line.set_ydata(samples[:,j])
+            ax.draw_artist(line)
+            fig.canvas.blit(ax.bbox)
+    
+    
+    # helper method for updating a single plot
+    
     def on_pause_button(self, event):
         self.paused = not self.paused
     
     def on_update_pause_button(self, event):
         label = "Resume" if self.paused else "Pause"
         self.pause_button.SetLabel(label)
-    
-    def init_plot(self, width=3.2, length=1.8):
-        self.dpi = 100
-        self.fig = Figure((width, length), dpi=self.dpi)
-        #self.plot = plot(
+        
+# A class that reads from the serial port using an isolated thread
+class serial_reader_thread(threading.Thread):
+    # Cookie-cutter __init__ function; nothing special
+    def __init__(self, threadID, data):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.data = data
+    def run(self):
+        self.ser = serial.Serial(4, baudrate=57600, timeout=1)
+        time.sleep(5)
+        self.ser.write("Begin!")
+        # Run until turned off
+        while on:
+            # Read bytes in chunks of meaningful size
+            if self.ser.inWaiting() > 4:
+                b = bytearray(4)
+                self.ser.readinto(b)
+                # Don't read and write data simultaneously; acquire lock
+                dataLock.acquire()
+                data.append(b)
+                dataLock.release()
+        # Print status and close port on exit
+        self.ser.write("Stop!")
+        dataLock.acquire()
+        print "Data:", len(data)
+        dataLock.release()
+        print "Serial:", self.ser.inWaiting()
+        self.ser.close()
         
 app = wx.App(False)
 frame = MainFrame(None, wx.ID_ANY, "Sonic Oxen")
