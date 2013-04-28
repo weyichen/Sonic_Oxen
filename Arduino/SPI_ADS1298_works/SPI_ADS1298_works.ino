afe8100bd2b5010cbc6dc8b549f3c5a006478c45
Daniel Wei <weyichen@seas.upenn.edu>
2013-04-28 04:31:51 -0400

    :'( :'(

--- Arduino/SPI_ADS1298_works/SPI_ADS1298_works.ino
+++ Arduino/SPI_ADS1298_works/SPI_ADS1298_works.ino
@@ -1,12 +1,23 @@
 // include the SPI library:
 #include <SPI.h>
 
-String start_code = "Begin!";
-String stop_code = "Stop!";
+/* Operational Modes
+0 - read initial registers only
+1 - check noise
+2 - square-wave signal test
+3 - regular input mode
+*/
+const int op_mode = 3;
+
+// use xbee transmission 0 / 1
+const int xbee = 1;
+
+// start() or print to serial monitor
+const int debug = 0;
 
 const int clk_div = 21;
-const int ss = 10;
 
+const int ss = 10;
 const int START = 11;
 const int DRDY = 9;
 const int PDWN = 8;
@@ -14,18 +25,56 @@ const int RESET = 8;
 
 byte regs[26];
 
+byte drdy_temp;
+byte drdy_val;
+byte d_out[27];
+
+byte wait;
+
 // Read registers
 void read_regs() {  
   SPI.transfer(ss, 32, SPI_CONTINUE); // RREG start at reg 0
   SPI.transfer(ss, 25, SPI_CONTINUE); // RREG 26 registers  
   for (int i=0; i<26; i++) regs[i] = SPI.transfer(ss, 0, SPI_CONTINUE);
   digitalWrite(ss, HIGH);
-  for (int i=0; i<26; i++) Serial1.println(regs[i]);
+  for (int i=0; i<26; i++) {
+    Serial.print("R");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(regs[i]);
+  }
+}
+
+boolean is_start_code(char code[]) {
+  if (code[0] == 'B' && code[1] == 'e' &&
+    code[2] == 'g' && code[3] == 'i' &&
+    code[4] == 'n' && code[5] == '!')
+    return true;
+  return false;
+}
+
+boolean is_stop_code(char code[]) {
+  if (code[0] == 'S' && code[1] == 't' &&
+    code[2] == 'o' && code[3] == 'p' &&
+    code[4] == '!')
+    return true;
+  return false;
+}
+
+void get_data() {
+  for (int i=0; i<27; i++) d_out[i] = SPI.transfer(ss, 0, SPI_CONTINUE);
+    if (!debug) {
+      for (int i=0; i<27; i++) Serial1.write(d_out[i]);
+    } else if (debug) {
+      for (int i=0; i<27; i++) Serial.println(d_out[i]);
+    }
+    digitalWrite(ss, HIGH);
 }
 
 void setup() {
   // put your setup code here, to run once:
   
+  if (debug == 1) Serial.begin(9600);
   Serial1.begin(57600);
   pinMode(START, OUTPUT);
   pinMode(PDWN, OUTPUT);
@@ -56,46 +105,65 @@ void setup() {
   delayMicroseconds(1);
   digitalWrite(ss, HIGH);
   
-  read_regs();
+  if (debug == 1) read_regs();
   
-  // Write setting registers
-  // WREG CONFIG1 0x86
   // WREG CONFIG1 0x86
   SPI.transfer(ss, 65, SPI_CONTINUE); // start at register 1
-  SPI.transfer(ss, 1, SPI_CONTINUE); // # regs to write - 1
+  SPI.transfer(ss, 0, SPI_CONTINUE); // # regs to write - 1
   SPI.transfer(ss, 134, SPI_CONTINUE); // set in HR Mode and DR = f /1024 (x86)
-  SPI.transfer(ss, 0); // set CONFIG2 to 0
+  delayMicroseconds(1);
+  digitalWrite(ss, HIGH);
   
-//  // Set All Channels to Input Short (1)
-//  SPI.transfer(ss, 69, SPI_CONTINUE); // start at register 5
-//  SPI.transfer(ss, 7, SPI_CONTINUE); // # write 8 registers
-//  for (int i=0; i<8; i++) SPI.transfer(ss, 1, SPI_CONTINUE);
-//  digitalWrite(ss, HIGH);
+  if (op_mode == 1) {
+    // WREG CONFIG2 0x00
+    SPI.transfer(ss, 66, SPI_CONTINUE); // start at register 1
+    SPI.transfer(ss, 0, SPI_CONTINUE); // set CONFIG2 to 0
+    SPI.transfer(ss, 0, SPI_CONTINUE);
+    delayMicroseconds(1);
+    digitalWrite(ss, HIGH);
+    
+    // Set All Channels to Input Short (1)
+    SPI.transfer(ss, 69, SPI_CONTINUE); // start at register 5
+    SPI.transfer(ss, 7, SPI_CONTINUE); // # write 8 registers
+    for (int i=0; i<8; i++) SPI.transfer(ss, 1, SPI_CONTINUE);
+    digitalWrite(ss, HIGH);
+    
+    if (debug == 1) {
+      Serial.println("Noise Testing: New Register Values");
+      // Read registers again
+      read_regs();
+    }
+  }
   
-  Serial1.println("NEW VALUES");
-  // Read registers again
-  read_regs();
+  if (op_mode == 2) {
+    // WREG CONFIG2 0x10
+    SPI.transfer(ss, 66, SPI_CONTINUE); // start at register 2
+    SPI.transfer(ss, 0, SPI_CONTINUE);
+    SPI.transfer(ss, 16, SPI_CONTINUE);
+    delayMicroseconds(1);
+    digitalWrite(ss, HIGH);
+    
+    // Activate a 1mV x Vref/2.4) square-wave test signal on all channels (5)
+    SPI.transfer(ss, 69, SPI_CONTINUE); // start at register 5
+    SPI.transfer(ss, 7, SPI_CONTINUE); // # write 8 registers
+    for (int i=0; i<8; i++) SPI.transfer(ss, 5, SPI_CONTINUE);
+    digitalWrite(ss, HIGH);
+    
+    // Read registers again
+    if (debug == 1) {
+      Serial.println("Square-Wave Testing: New Register Values");
+      read_regs();
+    }
+  }
   
-  // Set test signals
-  // SDATAC
-//  SPI.transfer(ss, 17, SPI_CONTINUE);
-//  delayMicroseconds(1);
-//  digitalWrite(ss, HIGH);
-//  
-//  // WREG CONFIG2 0x10
-//  SPI.transfer(ss, 66, SPI_CONTINUE); // start at register 2
-//  SPI.transfer(ss, 0, SPI_CONTINUE);
-//  SPI.transfer(ss, 16);
-//  
-//  Serial1.println("Ready to test square wave");
-//  
-//  // Activate a 1mV x Vref/2.4) square-wave test signal on all channels (5)
-//  SPI.transfer(ss, 69, SPI_CONTINUE); // start at register 5
-//  SPI.transfer(ss, 7, SPI_CONTINUE); // # write 8 registers
-//  for (int i=0; i<8; i++) SPI.transfer(ss, 5, SPI_CONTINUE);
-//  digitalWrite(ss, HIGH);
+  if (op_mode == 3) {
+    if (debug == 1) {
+      Serial.println("Regular Input mode");
+      read_regs();
+    }  
+  }
   
-    // Set START HIGH
+  // Set START HIGH
   digitalWrite(START, HIGH);
   delay(5);
 
@@ -104,82 +172,77 @@ void setup() {
   delayMicroseconds(1);
   digitalWrite(ss, HIGH);
   
-  byte drdy_temp = 1;
-  byte drdy_val = 1;
-  byte d_out[27];
+  drdy_temp = 1;
+  drdy_val = 1;
   for (int i=0; i<27; i++) d_out[i] = 0;
   
- Serial1.println("Ready to check noise");
+  if (debug == 1) Serial.println("Ready to collect data!");
   
-  // Capture data and check noise
-  // Look for DRDY and issue 24 + n * 24 SCLKs
-  int counter = 0;
+  if (xbee) wait = 1;
   
-   byte wait = 1;
-   // Wait until asked to start
-   while (wait) {
-     if (Serial1.available() >= 6) {
-       char cmd[7];
-       cmd[6] = '\0';
-       for (int i = 0; i < 6; i++)  {
-         cmd[i] = Serial1.read();
-       }
-       if (start_code.equals(cmd)) {
-         wait = 0;
-       }
-       else {
-         // Flush input and retry
-         while (Serial1.available()) {
-           Serial1.read();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+   if (xbee) {
+     // Wait until asked to start
+     while (wait) {
+       if (Serial1.available() >= 6) {
+         char cmd[7];
+         cmd[6] = '\0';
+         for (int i = 0; i < 6; i++)  {
+           cmd[i] = Serial1.read();
          }
-       }
-     }
-   }
-  
-  while (counter < 100000) {
-   drdy_temp = digitalRead(DRDY);
-   
-   // if drdy has toggled
-   if (drdy_temp != drdy_val) {
-    drdy_val = drdy_temp;
-    
-    // if toggled to low, data is available
-    if (drdy_val == 0) {
-      // SPI.transfer(ss, 18);
-      for (int i=0; i<27; i++) d_out[i] = SPI.transfer(ss, 0, SPI_CONTINUE);
-      digitalWrite(ss, HIGH);
-      
-      for (int i=0; i<27; i++) Serial1.write(d_out[i]);
-      //
-      counter++;
-    }
-   }
-   
-    if (Serial1.available() >= 5) {
-       char cmd[6];
-       cmd[5] = '\0';
-       for (int i = 0; i < 5; i++)  {
-         cmd[i] = Serial1.read();
-       }
-       if (stop_code.equals(cmd)) {
-         wait = 1;
-       }
-       // Flush input if not appropriate
-       else {
-         Serial1.println();
-         while(Serial1.available()) {
-           Serial1.write(Serial1.read());
+         if (is_start_code(cmd)) {
+           wait = 0;
+         }
+         else {
+           Serial1.println(cmd);
+           // Flush input and retry
+           while (Serial1.available()) {
+             Serial1.println(Serial1.read());
+           }
          }
        }
      }
   }
   
-  // Capture Data and Test Signal
-  // Look for DRDY and issue 24 + n * 24 SCLKs
- 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly: 
+ // check DRDY and send data to XBee
+  drdy_temp = digitalRead(DRDY);
+  if (drdy_temp != drdy_val) {
+    drdy_val = drdy_temp;
+    if (!drdy_val) {
+      for (int i=0; i<27; i++) d_out[i] = SPI.transfer(ss, 0, SPI_CONTINUE);
+      if (!debug) {
+        for (int i=0; i<27; i++) Serial1.write(d_out[i]);
+      } else if (debug) {
+        for (int i=0; i<27; i++) Serial.println(d_out[i]);
+      }
+      digitalWrite(ss, HIGH);
+    }
+  }
   
+   if (xbee) {
+   if (!wait) {
+  if (Serial1.available() >= 5) {
+     char cmd[6];
+     cmd[5] = '\0';
+     for (int i = 0; i < 5; i++)  {
+     cmd[i] = Serial1.read();
+     }
+     if (is_stop_code(cmd)) {
+     wait = 1;
+     }
+   // Flush input if not appropriate
+     else {
+     Serial1.println();
+     while(Serial1.available()) {
+     Serial1.write(Serial1.read());
+     }
+     }
+   }
+  }
+  } 
+   
 }
